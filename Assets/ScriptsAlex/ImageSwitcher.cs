deaf8778adec9b11d467966cf6bca3790205e57f
Alexandre Kabil <alexandre.kabil@gmail.com>
2023-09-24 23:25:22 +0200

    test commit github dev

---
+++ Assets/ScriptsAlex/ImageSwitcher.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ImageSwitcher : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private List<Transform> childrenimages = new List<Transform>();
+    private const int numofChild = 2;
+
+    private bool initOK = false;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        bool init = true;
+        foreach(Transform t in transform)
+        {
+            childrenimages.Add(t);
+        }
+        while (init)
+        {
+            if (childrenimages.Count == numofChild)
+            {
+                init= false;
+                yield return null;
+            }
+            else
+            {
+                Debug.Log("pas le bon nombre d'images");
+                yield return null;
+            }
+            yield return null;
+        }
+        initOK = true;
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (initOK)
+        {
+            childrenimages[1].gameObject.SetActive(false);
+            childrenimages[0].gameObject.SetActive(true);
+        }
+        Debug.Log("dedans");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+        if (initOK)
+        {
+            childrenimages[1].gameObject.SetActive(true);
+            childrenimages[0].gameObject.SetActive(false);
+        }
+        Debug.Log("dehors");
+    }
+
+}
---
+++ Assets/ScriptsAlex/SimpleDissolve.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDissolve : MonoBehaviour
+{
+    // à la main car flemme ici
+    public Renderer DissolveRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(DissolveOverTime(2.0f));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var value = Mathf.PingPong(Time.time * 0.2f, 1f);
+        DissolveRenderer.material.SetFloat("_Dissolve", value);
+    }
+
+    public IEnumerator DissolveOverTime(float f)
+    {
+        float timer = 0;
+        float value = 0;
+        bool init = true;
+
+        while (init)
+        {
+            if (timer <= 0)
+            {
+                timer += Time.deltaTime;
+                value = Mathf.Lerp(value, 1, Time.deltaTime);
+                DissolveRenderer.material.SetFloat("_Dissolve", value);
+                yield return null;
+            }
+            
+            if (timer >= f)
+            {
+                timer -= Time.deltaTime;
+                value = Mathf.Lerp(value, 0, Time.deltaTime);
+                DissolveRenderer.material.SetFloat("_Dissolve", value);
+                yield return null;
+            }
+        }
+
+        yield return null;
+    }
+}
